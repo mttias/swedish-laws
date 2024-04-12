bb75727438d0d2d98ad91a803084951af1d1f8c5
Simon <76711104+mttias@users.noreply.github.com>
2024-04-12 15:25:26 +0200

    feat(repo): added all files

--- index.ts
+++ index.ts
@@ -1,15 +1,3 @@
-/* import { size } from "./src/utils/size/size";
-import { toHTML } from "./src/utils/toHTML";
-import axios from "axios";
-
-const url = (id: string) =>
-	`https://rkrattsbaser.gov.se/sfst?fritext=&upph=false&post_id=${id}`;
-
-const response = await axios.get(url("1"));
-console.log(size(response));
-await toHTML(response.data, "data.xml");
- */
-
 import { cachedRequestWithId } from "./src/utils/cachedRequest/cachedRequest";
 
 for (let i = 0; i < 4700; i++) {
